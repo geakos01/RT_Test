36017d6cda1db833e014a31f81ad6f158a4c6205
geakos <geakos01@hotmail.hu>
2024-09-14 16:51:40 +0200

    Added FastAPI

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from fastapi import FastAPI, Request
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/echo', methods=['GET'])
-def echo():
-    text = request.args.get('text')
-    return f"The given text is: {text}"
+@app.get("/echo")
+async def echo(text: str = None):
+    return {"message": f"Pinging FastAPI {text}"}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
